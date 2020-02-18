0d9e61d715f99723ba59ebb1a281bc55c8fdad27
dip <dmpuchenkov@protonmail.com>
2020-02-18 11:08:02 +0300

    Made models

---
+++ go.mod
@@ -0,0 +1,8 @@
+module test_taskn
+
+go 1.13
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/mux v1.7.3
+)
--- internal/pkg/handler/models.go
+++ internal/pkg/handler/models.go
@@ -1,1 +1,53 @@
 package handler
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+var Loc *time.Location
+
+func init() {
+	Loc, _ = time.LoadLocation("Europe/Moscow")
+}
+
+const TimeFormat = time.RFC3339 //duplicate
+
+type Post struct {
+	ID     			uuid.UUID   `json:"id"     db:"id"`
+	PersonID 		uuid.UUID   `json:"person_id" db:"person_id"`
+	Header 			string      `json:"header" db:"header"`
+	Text   			string      `json:"text"   db:"text"`
+	Timestamp 		string 		`json:"timestamp" db:"timestamp"`
+	Address 		address  	`json:"address" db:"address"`
+	Comments 		[]comments  `json:"comments"`
+}
+
+type address struct {
+	Index 	 		int 		`json:"index"`
+	Country  		string 		`json:"country"`
+	Region   		string 		`json:"region"`
+	City     		string 		`json:"city"`
+	Street   		string 		`json:"street"`
+	Metro    		string 		`json:"metro"`
+	HouseNumber 	int 		`json:"houseNumber"`
+	Section 		string 		`json:"section"`
+	Flat 			string 		`json:"flat"`
+}
+
+type comments struct {
+	PostId 			uuid.UUID 	`json:"post_id" db:"post_id"`
+	ID 				uuid.UUID 	`json:"id" db:"id"`
+	Text 			string		`json:"text" db:"text"`
+	PersonId 		uuid.UUID 	`json:"person_id" db:"person_id"`
+}
+
+type Person struct {
+	ID 				uuid.UUID	`json:"id" db:"id"`
+	AvatarURL 		string		`json:"avatar_url" db:"avatar_url"`
+	Name 			string 		`json:"name" db:"name"`
+	Surname 		string		`json:"surname" db:"surname"`
+	Patronymic 		string 		`json:"patronymic" db:"patronymic"`
+	Address 		address  	`json:"address" db:"address"`
+}
+
