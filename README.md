ce98ddf73700fc51a79e7356a2d8d08b5df01250
dip <dmpuchenkov@protonmail.com>
2020-02-24 12:54:17 +0300

    Readme fix

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 Simple backend server
 
 To start project:
-1) Start postgreSQL on local machine 
-2) Add models from db/create_db.sql
-3) Insert data from db/insert_data.sql
-4) ```go run cmd/main/main.go```
+- Simply write in terminal: ``./docker-start.sh``
+- To stop project press Ctrl + C and run following script in terminal: ```./docker-stop.sh```
--- internal/pkg/db_connect/db_connect.go
+++ internal/pkg/db_connect/db_connect.go
@@ -1,7 +1,6 @@
 package db_connect
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -33,35 +32,4 @@ func InitDB(address, database string) *sqlx.DB{
 		fmt.Printf("Successfully connected to %v, database %v", host, database)
 	}
 	return Db
-}
-
-func InitDBSQL(address, database string) *sql.DB {
-	var err error
-
-	//dsn := "localhost:5432/some-postgres?"
-	//Db, err = sqlx.Open("postgres",
-	//	"postgres://"+address+"/"+database+"?sslmode=disable")
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	Database, err := sql.Open("postgres", psqlInfo)
-
-	if err != nil {
-		fmt.Println("Panic, while loading db")
-	}
-
-	if err := Db.Ping(); err != nil {
-		fmt.Println("Panic, while pinging db")
-	}
-
-	//Db, err := sqlx.Connect("postgres", "user=foo dbname=some-bar sslmode=disable")
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-
-	fmt.Println("Successfully connected to %v, database %v", address, database)
-
-
-	return Database
 }
